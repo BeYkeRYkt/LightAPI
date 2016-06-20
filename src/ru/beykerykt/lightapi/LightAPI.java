279025c0f94afdc52050112fe5292f9bd1a44724
BeYkeRYkt <beykerykt@gmail.com>
2016-06-20 12:47:34 +0900

    main: Update LightAPI

--- src/ru/beykerykt/lightapi/LightAPI.java
+++ src/ru/beykerykt/lightapi/LightAPI.java
@@ -1,30 +1,49 @@
 package ru.beykerykt.lightapi;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import ru.beykerykt.lightapi.chunks.ChunkCache;
 import ru.beykerykt.lightapi.chunks.ChunkInfo;
+import ru.beykerykt.lightapi.events.DeleteLightEvent;
+import ru.beykerykt.lightapi.events.SetLightEvent;
+import ru.beykerykt.lightapi.events.UpdateChunkEvent;
 import ru.beykerykt.lightapi.request.DataRequest;
 import ru.beykerykt.lightapi.request.RequestSteamMachine;
 import ru.beykerykt.lightapi.server.ServerModInfo;
 import ru.beykerykt.lightapi.server.ServerModManager;
-import ru.beykerykt.lightapi.server.nms.craftbukkit.NMSHandler_v1_9_R2;
+import ru.beykerykt.lightapi.server.nms.craftbukkit.CraftBukkit_v1_10_R1;
+import ru.beykerykt.lightapi.server.nms.craftbukkit.CraftBukkit_v1_8_R3;
+import ru.beykerykt.lightapi.server.nms.craftbukkit.CraftBukkit_v1_9_R2;
+import ru.beykerykt.lightapi.server.nms.paperspigot.PaperSpigot_v1_8_R3;
+import ru.beykerykt.lightapi.updater.Response;
+import ru.beykerykt.lightapi.updater.Updater;
+import ru.beykerykt.lightapi.updater.Version;
 import ru.beykerykt.lightapi.utils.Metrics;
 
-public class LightAPI extends JavaPlugin {
+public class LightAPI extends JavaPlugin implements Listener {
 
 	private static LightAPI plugin;
 	private static RequestSteamMachine machine;
+	private int configVer = 2;
+	private int update_delay_ticks;
+	private int max_iterations_per_tick;
 
 	@SuppressWarnings("static-access")
 	@Override
@@ -33,35 +52,96 @@ public class LightAPI extends JavaPlugin {
 		this.machine = new RequestSteamMachine();
 
 		ServerModInfo craftbukkit = new ServerModInfo("CraftBukkit");
-		craftbukkit.getVersions().put("v1_9_R2", NMSHandler_v1_9_R2.class);
-		// craftbukkit.getVersions().put("v1_7_R4", NMSHandler.class);
+		craftbukkit.getVersions().put("v1_8_R3", CraftBukkit_v1_8_R3.class);
+		craftbukkit.getVersions().put("v1_9_R2", CraftBukkit_v1_9_R2.class);
+		craftbukkit.getVersions().put("v1_10_R1", CraftBukkit_v1_10_R1.class);
 		ServerModManager.registerServerMod(craftbukkit);
 
 		ServerModInfo spigot = new ServerModInfo("Spigot");
-		spigot.getVersions().put("v1_9_R2", NMSHandler_v1_9_R2.class);
+		spigot.getVersions().put("v1_8_R3", CraftBukkit_v1_8_R3.class);
+		spigot.getVersions().put("v1_9_R2", CraftBukkit_v1_9_R2.class);
+		spigot.getVersions().put("v1_10_R1", CraftBukkit_v1_10_R1.class);
 		ServerModManager.registerServerMod(spigot);
 
 		ServerModInfo paperspigot = new ServerModInfo("PaperSpigot");
+		paperspigot.getVersions().put("v1_8_R3", PaperSpigot_v1_8_R3.class);
 		ServerModManager.registerServerMod(paperspigot);
 
 		ServerModInfo paper = new ServerModInfo("Paper");
+		paper.getVersions().put("v1_9_R2", CraftBukkit_v1_9_R2.class);
+		paper.getVersions().put("v1_10_R1", CraftBukkit_v1_10_R1.class);
 		ServerModManager.registerServerMod(paper);
 
 		ServerModInfo tacospigot = new ServerModInfo("TacoSpigot");
+		// tacospigot.getVersions().put("v1_8_R3", PaperSpigot_v1_8_R3.class); - call errors with anti-xray - obfuscate
+		tacospigot.getVersions().put("v1_9_R2", CraftBukkit_v1_9_R2.class);
+		tacospigot.getVersions().put("v1_10_R1", CraftBukkit_v1_10_R1.class);
 		ServerModManager.registerServerMod(tacospigot);
 	}
 
 	@Override
 	public void onEnable() {
+		// Config
+		try {
+			FileConfiguration fc = getConfig();
+			File file = new File(getDataFolder(), "config.yml");
+			if (file.exists()) {
+				if (fc.getInt("version") < configVer) {
+					file.delete(); // got a better idea?
+					generateConfig(file);
+				}
+			} else {
+				generateConfig(file);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Init config
+		this.update_delay_ticks = getConfig().getInt("update-delay-ticks");
+		this.max_iterations_per_tick = getConfig().getInt("max-iterations-per-tick");
+
+		// init nms
 		ServerModManager.init();
 		machine.start(2, 40); // TEST
+		getServer().getPluginManager().registerEvents(this, this);
 
+		// Starting updater
+		Bukkit.getScheduler().runTaskLaterAsynchronously(this, new Runnable() {
+			@Override
+			public void run() {
+				Version version = Version.parse(getDescription().getVersion());
+				String repo = "BeYkeRYkt/LightAPI";
+
+				Updater updater;
+				try {
+					updater = new Updater(version, repo);
+
+					Response response = updater.getResult();
+					if (response == Response.SUCCESS) {
+						log(Bukkit.getConsoleSender(), ChatColor.GREEN + "New update is available: " + ChatColor.YELLOW + updater.getLatestVersion() + ChatColor.GREEN + "!");
+						log(Bukkit.getConsoleSender(), ChatColor.GREEN + "Changes: ");
+						getServer().getConsoleSender().sendMessage(updater.getChanges());// for normal view
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}, 60);
+
+		// init metrics
 		try {
 			Metrics metrics = new Metrics(this);
 			metrics.start();
 		} catch (IOException e) {
 			// nothing...
 		}
+	}
+
+	@Override
+	public void onDisable() {
+		machine.shutdown();
+		ChunkCache.CHUNK_INFO_QUEUE.clear();
 	}
 
 	public void log(CommandSender sender, String message) {
@@ -72,45 +152,65 @@ public class LightAPI extends JavaPlugin {
 		return plugin;
 	}
 
+	public static boolean createLight(Location location, int lightlevel, boolean async) {
+		return createLight(location.getWorld(), location.getBlockX(), location.getBlockY(), location.getBlockZ(), lightlevel, async);
+	}
+
 	public static boolean createLight(final World world, final int x, final int y, final int z, final int lightlevel, boolean async) {
 		if (getInstance().isEnabled()) {
-			Block adjacent = getAdjacentAirBlock(world.getBlockAt(x, y, z));
+			final SetLightEvent event = new SetLightEvent(world, x, y, z, lightlevel, async);
+			Bukkit.getPluginManager().callEvent(event);
+
+			Block adjacent = getAdjacentAirBlock(world.getBlockAt(event.getX(), event.getY(), event.getZ()));
 			final int lx = adjacent.getX();
 			final int ly = adjacent.getY();
 			final int lz = adjacent.getZ();
 
-			if (async) {
+			if (event.isAsync()) {
 				machine.addToQueue(new DataRequest() {
 					@Override
 					public void process() {
-						ServerModManager.getNMSHandler().createLight(world, x, y, z, lightlevel);
-						ServerModManager.getNMSHandler().recalculateLight(world, lx, ly, lz);
+						ServerModManager.getNMSHandler().createLight(event.getWorld(), event.getX(), event.getY(), event.getZ(), event.getLightLevel());
+						ServerModManager.getNMSHandler().recalculateLight(event.getWorld(), lx, ly, lz);
 					}
 				});
 				return true;
 			}
-			ServerModManager.getNMSHandler().createLight(world, x, y, z, lightlevel);
-			ServerModManager.getNMSHandler().recalculateLight(world, lx, ly, lz);
+			ServerModManager.getNMSHandler().createLight(event.getWorld(), event.getX(), event.getY(), event.getZ(), event.getLightLevel());
+			ServerModManager.getNMSHandler().recalculateLight(event.getWorld(), lx, ly, lz);
 			return true;
 		}
 		return false;
 	}
 
+	public static boolean deleteLight(Location location, boolean async) {
+		return deleteLight(location.getWorld(), location.getBlockX(), location.getBlockY(), location.getBlockZ(), async);
+	}
+
 	public static boolean deleteLight(final World world, final int x, final int y, final int z, boolean async) {
 		if (getInstance().isEnabled()) {
-			if (async) {
+			final DeleteLightEvent event = new DeleteLightEvent(world, x, y, z, async);
+			Bukkit.getPluginManager().callEvent(event);
+			if (event.isAsync()) {
 				machine.addToQueue(new DataRequest() {
 					@Override
 					public void process() {
-						ServerModManager.getNMSHandler().deleteLight(world, x, y, z);
+						ServerModManager.getNMSHandler().deleteLight(event.getWorld(), event.getX(), event.getY(), event.getZ());
 					}
 				});
 				return true;
 			}
-			ServerModManager.getNMSHandler().deleteLight(world, x, y, z);
+			ServerModManager.getNMSHandler().deleteLight(event.getWorld(), event.getX(), event.getY(), event.getZ());
 			return true;
 		}
 		return false;
+	}
+
+	public static List<ChunkInfo> collectChunks(Location location) {
+		if (getInstance().isEnabled()) {
+			return ServerModManager.getNMSHandler().collectChunks(location.getWorld(), location.getBlockX(), location.getBlockY(), location.getBlockZ());
+		}
+		return null;
 	}
 
 	public static List<ChunkInfo> collectChunks(final World world, final int x, final int y, final int z) {
@@ -122,19 +222,24 @@ public class LightAPI extends JavaPlugin {
 
 	public static boolean updateChunks(ChunkInfo info) {
 		if (getInstance().isEnabled()) {
-			int maxY = info.getChunkYHeight();
-			if (ChunkCache.CHUNK_INFO_QUEUE.contains(info)) {
-				int index = ChunkCache.CHUNK_INFO_QUEUE.indexOf(info);
+			UpdateChunkEvent event = new UpdateChunkEvent(info);
+			Bukkit.getPluginManager().callEvent(event);
+			if (ChunkCache.CHUNK_INFO_QUEUE.contains(event.getChunkInfo())) {
+				int index = ChunkCache.CHUNK_INFO_QUEUE.indexOf(event.getChunkInfo());
 				ChunkInfo previous = ChunkCache.CHUNK_INFO_QUEUE.get(index);
-				if (previous.getChunkYHeight() > maxY) {
-					maxY = previous.getChunkYHeight();
+				if (previous.getChunkYHeight() > event.getChunkInfo().getChunkYHeight()) {
+					event.getChunkInfo().setChunkYHeight(previous.getChunkYHeight());
 				}
 				ChunkCache.CHUNK_INFO_QUEUE.remove(index);
 			}
-			ChunkCache.CHUNK_INFO_QUEUE.add(info);
+			ChunkCache.CHUNK_INFO_QUEUE.add(event.getChunkInfo());
 			return true;
 		}
 		return false;
+	}
+
+	public static boolean updateChunks(Location location, Collection<? extends Player> players) {
+		return updateChunks(location.getWorld(), location.getBlockX(), location.getBlockY(), location.getBlockZ(), players);
 	}
 
 	public static boolean updateChunks(World world, int x, int y, int z, Collection<? extends Player> players) {
@@ -161,5 +266,63 @@ public class LightAPI extends JavaPlugin {
 			}
 		}
 		return block;
+	}
+
+	private void generateConfig(File file) {
+		FileConfiguration fc = getConfig();
+		if (!file.exists()) {
+			fc.options().header("LightAPI v" + getDescription().getVersion() + " Configuration" + "\nby BeYkeRYkt");
+			fc.set("version", configVer);
+			fc.set("update-delay-ticks", 2);
+			fc.set("max-iterations-per-tick", 40);
+			saveConfig();
+		}
+	}
+
+	public int getUpdateDelayTicks() {
+		return update_delay_ticks;
+	}
+
+	public void setUpdateDelayTicks(int update_delay_ticks) {
+		this.update_delay_ticks = update_delay_ticks;
+	}
+
+	public int getMaxIterationsPerTick() {
+		return max_iterations_per_tick;
+	}
+
+	public void setMaxIterationsPerTick(int max_iterations_per_tick) {
+		this.max_iterations_per_tick = max_iterations_per_tick;
+	}
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		final Player player = event.getPlayer();
+
+		if (player.hasPermission("lightapi.updater")) {
+			Bukkit.getScheduler().runTaskLaterAsynchronously(this, new Runnable() {
+
+				@Override
+				public void run() {
+					Version version = Version.parse(getDescription().getVersion());
+					String repo = "BeYkeRYkt/LightAPI";
+
+					Updater updater;
+					try {
+						updater = new Updater(version, repo);
+
+						Response response = updater.getResult();
+						if (response == Response.SUCCESS) {
+							log(player, ChatColor.GREEN + "New update is available: " + ChatColor.YELLOW + updater.getLatestVersion() + ChatColor.GREEN + "!");
+							log(player, ChatColor.GREEN + "Changes: ");
+							player.sendMessage(updater.getChanges());// for normal view
+							// player.playSound(player.getLocation(), Sound.LEVEL_UP, 1, 1);
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}, 60);
+		}
 	}
 }
