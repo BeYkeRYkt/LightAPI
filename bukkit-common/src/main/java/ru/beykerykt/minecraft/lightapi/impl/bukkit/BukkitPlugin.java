bf15e4c2a2e69febeed0c6dd32c398c1cbfa3cba
BeYkeRYkt <beykerykt@gmail.com>
2019-03-21 00:26:40 +0700

    LightAPI: bukkit-common: implement commands for players and console
    Change-Id: I5cddbb47877a7a8e2679558785ee29e906689107

--- bukkit-common/src/main/java/ru/beykerykt/minecraft/lightapi/impl/bukkit/BukkitPlugin.java
+++ bukkit-common/src/main/java/ru/beykerykt/minecraft/lightapi/impl/bukkit/BukkitPlugin.java
@@ -27,7 +27,11 @@ package ru.beykerykt.minecraft.lightapi.impl.bukkit;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -36,6 +40,11 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import ru.beykerykt.minecraft.lightapi.common.Build;
 import ru.beykerykt.minecraft.lightapi.common.IChunkData;
 import ru.beykerykt.minecraft.lightapi.common.LightAPI;
 import ru.beykerykt.minecraft.lightapi.common.LightType;
@@ -55,13 +64,108 @@ public class BukkitPlugin extends JavaPlugin implements Listener {
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 
-	private boolean flag = true;
+	public void log(CommandSender sender, String message) {
+		sender.sendMessage(ChatColor.AQUA + "<LightAPI>: " + ChatColor.WHITE + message);
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("lightapi")) {
+			if (sender instanceof Player) {
+				Player player = (Player) sender;
+				if (args.length == 0) {
+					player.sendMessage(ChatColor.AQUA + " ------- <LightAPI " + ChatColor.WHITE
+							+ getDescription().getVersion() + "> ------- ");
+
+					TextComponent version = new TextComponent(ChatColor.AQUA + " Current version: ");
+					TextComponent update = new TextComponent(ChatColor.WHITE + getDescription().getVersion());
+					update.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lightapi update"));
+					update.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+							new ComponentBuilder("Click here for check update").create()));
+					version.addExtra(update);
+					player.spigot().sendMessage(version);
+
+					player.sendMessage(ChatColor.AQUA + " Server name: " + ChatColor.WHITE + getServer().getName());
+					player.sendMessage(
+							ChatColor.AQUA + " Server version: " + ChatColor.WHITE + getServer().getVersion());
+
+					TextComponent text = new TextComponent(" | ");
+					TextComponent sourcecode = new TextComponent(ChatColor.AQUA + "Source code");
+					sourcecode.setClickEvent(
+							new ClickEvent(ClickEvent.Action.OPEN_URL, "http://github.com/BeYkeRYkt/LightAPI/"));
+					sourcecode.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+							new ComponentBuilder("Goto the GitHub!").create()));
+					text.addExtra(sourcecode);
+					text.addExtra(new TextComponent(ChatColor.WHITE + " | "));
+
+					TextComponent developer = new TextComponent(ChatColor.AQUA + "Developer");
+					developer.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "http://github.com/BeYkeRYkt/"));
+					developer.setHoverEvent(
+							new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("BeYkeRYkt").create()));
+					text.addExtra(developer);
+					text.addExtra(new TextComponent(ChatColor.WHITE + " | "));
+
+					TextComponent contributors = new TextComponent(ChatColor.AQUA + "Contributors");
+					contributors.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL,
+							"https://github.com/BeYkeRYkt/LightAPI/graphs/contributors"));
+					contributors.setHoverEvent(
+							new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("ALIENS!!").create()));
+					text.addExtra(contributors);
+					text.addExtra(new TextComponent(ChatColor.WHITE + " | "));
+
+					player.spigot().sendMessage(text);
+
+					TextComponent licensed = new TextComponent(" Licensed under ");
+					TextComponent MIT = new TextComponent(ChatColor.AQUA + "MIT License");
+					MIT.setClickEvent(
+							new ClickEvent(ClickEvent.Action.OPEN_URL, "https://opensource.org/licenses/MIT/"));
+					MIT.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+							new ComponentBuilder("Goto for information about license!").create()));
+					licensed.addExtra(MIT);
+					player.spigot().sendMessage(licensed);
+
+					if (Build.CURRENT_VERSION == Build.VERSION_CODES.ONE) {
+						player.sendMessage(ChatColor.BLACK + "backwards compatibility enabled");
+					}
+
+				} else {
+					log(player, ChatColor.RED + "Hmm... This command does not exist. Are you sure write correctly ?");
+				}
+			} else if (sender instanceof ConsoleCommandSender) {
+				ConsoleCommandSender console = (ConsoleCommandSender) sender;
+				if (args.length == 0) {
+					console.sendMessage(ChatColor.AQUA + " ------- <LightAPI " + ChatColor.WHITE
+							+ getDescription().getVersion() + "> ------- ");
+					console.sendMessage(
+							ChatColor.AQUA + " Current version: " + ChatColor.WHITE + getDescription().getVersion());
+					console.sendMessage(ChatColor.AQUA + " Server name: " + ChatColor.WHITE + getServer().getName());
+					console.sendMessage(
+							ChatColor.AQUA + " Server version: " + ChatColor.WHITE + getServer().getVersion());
+					console.sendMessage(ChatColor.AQUA + " Source code: " + ChatColor.WHITE
+							+ "http://github.com/BeYkeRYkt/LightAPI/");
+					console.sendMessage(ChatColor.AQUA + " Developer: " + ChatColor.WHITE + "BeYkeRYkt");
+					console.sendMessage("");
+					console.sendMessage(ChatColor.WHITE + " Licensed under: " + ChatColor.AQUA + "MIT License");
+
+					if (Build.CURRENT_VERSION == Build.VERSION_CODES.ONE) {
+						console.sendMessage(ChatColor.BLACK + "backwards compatibility enabled");
+					}
+				} else {
+					log(console, ChatColor.RED + "Hmm... This command does not exist. Are you sure write correctly ?");
+				}
+			}
+		}
+		return true;
+	}
 
 	// testing
+	private boolean debug = true;
+	private boolean flag = true;
+
 	@EventHandler
 	public void onPlayerClick(PlayerInteractEvent event) {
 		Player p = event.getPlayer();
-		if (!flag)
+		if (!flag || !debug)
 			return;
 		if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 			List<IChunkData> list = new CopyOnWriteArrayList<IChunkData>();
@@ -99,6 +203,8 @@ public class BukkitPlugin extends JavaPlugin implements Listener {
 
 	@EventHandler
 	public void onPlayerJoin(AsyncPlayerChatEvent event) {
+		if (!flag || !debug)
+			return;
 		Player p = event.getPlayer();
 		if (event.getMessage().equals("light")) {
 			p.sendMessage("At player location");
